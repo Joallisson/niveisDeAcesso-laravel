9e1214d50dd4bab9887d380980516ed7d81cbc94
joallisson <joallissonnc@gmail.com>
2022-06-25 11:19:56 -0300

    inital commit

---
+++ app/Http/Middleware/AdminAccess.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->check() AND auth()->user()->admin) { //Se o usuário estiver autenticado e for admin
+            return $next($request);
+        }
+
+        dd('Acesso Negado, Você não é um administrador');
+        
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('admin')->group(function(){
+    Route::get('admin', function(){
+        dd('Você é um Admin');
+    });
+});
+
+Route::middleware('client')->group(function(){
+    Route::get('client', function(){
+        dd('Você é um Cliente');
+    });
+});
